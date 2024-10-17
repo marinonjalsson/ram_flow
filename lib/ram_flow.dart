2cfd803058304cd2a4e44ac77a3d84ac3ab6cb29
Marinó á skrifbord <marino.njalsson@gmail.com>
2024-10-17 10:09:56 +0000

    fix.

--- lib/ram_flow.dart
+++ lib/ram_flow.dart
@@ -3,8 +3,10 @@ library ram_flow;
 
 export 'src/builder.dart';
 export 'src/ctl.dart';
-export 'src/pipe.dart';
+export 'src/data.dart';
 export 'src/flow.dart';
+export 'src/func.dart';
+export 'src/pipe.dart';
 export 'src/ram_list.dart';
 export 'src/ui.dart';
 export 'src/update.dart';
